f2c238c45c1f01973117c7e8d385eea0dc5e72d1
Emmanuel <emmanuel.mukira@student.moringaschool.com>
2023-03-19 16:36:58 +0300

    Trying to get speed to work

--- speedDetector.js
+++ speedDetector.js
@@ -1,15 +1,19 @@
 //Code to detect speed
-//Variables definition
-let speed ;
-const speedLimit=70;
-const distancePerDemeritPoint=5;
-//Function to test input speed and provide relevant output
-function detectSpeed(){
-    if(speed<=speedLimit){
-        return "Ok."
+    //Variables definition
+    let speed=document.getElementById('speed')
+    const speedLimit=70;
+    const distancePerDemeritPoint=5;
+    var message=document.getElementById('showMessage');
+    //Function to test input speed and output either a message or one's demerit points
+    function detectSpeed(){
+        const speedValue=parseInt(speed.value);
+    if(speedValue<=speedLimit){
+         message="Ok."
     }
     else {
-        const demeritPoints=Math.floor((speed-speedLimit)/kmPerDemeritPoint);
-        console.log(`These are your demerit points:${demeritPoints}`);
+        var demeritPoints=Math.floor((speed-speedLimit)/kmPerDemeritPoint);
+         message=`These are your demerit points:${demeritPoints}`;
     }
+    console.log(message);
+    showMessage.innerText=`${message}`;
 }
